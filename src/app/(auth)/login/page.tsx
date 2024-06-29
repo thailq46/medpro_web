340e646173cb08393c36d6a3693cb0824c7a8208
thailq <xinphatmutikill@gmail.com>
2024-06-30 01:21:25 +0700

    Switching component Login to static rendering

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,6 @@
-import {AT_COOKIE_NAME} from "@/apiRequest/common";
 import dynamic from "next/dynamic";
-import {cookies} from "next/headers";
 const Login = dynamic(() => import("@/module/login"));
 
-export default async function index() {
-  const cookieStorage = cookies();
-  const isLogin = Boolean(cookieStorage.get(AT_COOKIE_NAME));
-  if (isLogin) {
-    const {redirect} = await import("next/navigation");
-    redirect("/");
-  }
+export default function index() {
   return <Login />;
 }
