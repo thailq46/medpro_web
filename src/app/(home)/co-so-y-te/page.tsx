94a5ab79c31266d0b98c247c4dff911e04e38052
thailq <xinphatmutikill@gmail.com>
2024-07-04 21:25:10 +0700

    Test metadata

--- src/app/(home)/co-so-y-te/page.tsx
+++ src/app/(home)/co-so-y-te/page.tsx
@@ -1,5 +1,12 @@
+import {Metadata} from "next";
 import dynamic from "next/dynamic";
 const HealthFacilities = dynamic(() => import("@/module/health-facilities"));
+
+export const metadata: Metadata = {
+  title: "Danh sách cơ sở y tế ",
+  description:
+    "Danh sách các loại bệnh viện - cơ sở y tế. Được tạo bởi Lê Quang Thái",
+};
 
 export default function index() {
   return <HealthFacilities />;
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,6 +5,13 @@ import HomeHeader from "@/module/home/_component/HomeHeader";
 import HomeHospital from "@/module/home/_component/HomeHospital";
 import HomeNews from "@/module/home/_component/HomeNews";
 import HomeSupport from "@/module/home/_component/HomeSupport";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: "Đăng ký khám, đặt lịch khám hơn 100 bệnh viện lớn tại Việt Nam",
+  description:
+    "Medpro, đăng ký trực tuyến, tổng đài đặt lịch khám nhanh 19002115, lấy số thứ tự khám nhanh chóng và tiện lợi tại các Bệnh viện Đại học Y Dược TP HCM, Chợ Rẫy, Mắt TP HCM, Da liễu TP, Nhi Đồng 1. Được tạo bởi Lê Quang Thái",
+};
 
 export default async function Home({
   searchParams,
